3fe81124dd5118e2334f5dd7dc387722c9fd3e5c
Irving Meng <imeng@lcs.on.ca>
2024-05-23 11:37:54 -0400

    Added a view model to manage state within the app, and handle CRUD (create, read, update, delete) operations.

---
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListViewModel.swift
+//  TodoList
+//
+//  Created by 孟元森 on 2024-05-21.
+//
+
+import SwiftUI
+
+struct TodoListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListViewModel()
+}
