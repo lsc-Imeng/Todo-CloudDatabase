0ea5ac8cd2fbe72abe51c4bec702879a4b21ad01
Irving Meng <imeng@lcs.on.ca>
2024-05-23 11:51:14 -0400

    Made use of the view model within our view to manage state.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -17,8 +17,8 @@ struct LandingView: View {
     // The search text
     @State var searchText = ""
     
-    // The list of to-do items
-    @State var todos: [TodoItem] = exampleItems
+    // The view model
+    @State var viewModel = TodoListViewModel()
     
     // MARK: Computed properties
     var body: some View {
@@ -26,7 +26,7 @@ struct LandingView: View {
             
             VStack {
                 
-                List($todos) { $todo in
+                List($viewModel.todos) { $todo in
                     
                     ItemView(currentItem: $todo)
                         // Delete item
@@ -35,7 +35,7 @@ struct LandingView: View {
                                 "Delete",
                                 role: .destructive,
                                 action: {
-                                    delete(todo)
+                                    viewModel.delete(todo)
                                 }
                             )
                         }
@@ -48,7 +48,7 @@ struct LandingView: View {
                     
                     Button("ADD") {
                         // Add the new to-do item
-                        createToDo(withTitle: newItemDescription)
+                        viewModel.createToDo(withTitle: newItemDescription)
                     }
                     .font(.caption)
                 }
@@ -60,28 +60,7 @@ struct LandingView: View {
         }
     }
     
-    // MARK: Functions
-    func createToDo(withTitle title: String) {
-        
-        // Create the new to-do item instance
-        let todo = TodoItem(
-            title: title,
-            done: false
-        )
-        
-        // Append to the array
-        todos.append(todo)
-        
-    }
     
-    func delete(_ todo: TodoItem) {
-        
-        // Remove the provided to-do item from the array
-        todos.removeAll { currentItem in
-            currentItem.id == todo.id
-        }
-        
-    }
 }
 
 #Preview {
