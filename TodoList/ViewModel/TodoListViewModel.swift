84051a1bada7530cb2d4359c1e60e0a9774dd309
Irving Meng <imeng@lcs.on.ca>
2024-05-28 08:46:42 -0400

    Adjusted the view model so that newly added to-do items are persisted.

--- TodoList/ViewModel/TodoListViewModel.swift
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -40,15 +40,39 @@ class TodoListViewModel {
     // MARK: Functions
     func createToDo(withTitle title: String) {
         
-        // Create the new to-do item instance
-        let todo = TodoItem(
-            title: title,
-            done: false
-        )
-        
-        // Append to the array
-        todos.append(todo)
-        
+        // Create a unit of asynchronous work to add the to-do item
+        Task {
+            
+            // Create the new to-do item instance
+            // NOTE: The id will be nil for now
+            let todo = TodoItem(
+                title: title,
+                done: false
+            )
+            
+            // Write it to the database
+            do {
+                
+                // Insert the new to-do item, and then immediately select
+                // it back out of the database
+                let newlyInsertedItem: TodoItem = try await supabase
+                    .from("todos")
+                    .insert(todo)   // Insert the todo item created locally in memory
+                    .select()       // Select the item just inserted
+                    .single()       // Ensure just one row is returned
+                    .execute()      // Run the query
+                    .value          // Automatically decode the JSON into an instance of TodoItem
+
+                // Finally, insert the to-do item instance we just selected back from the
+                // database into the array used by the view model
+                // NOTE: We do this to obtain the id that is automatically assigned by Supabase
+                //       when the to-do item was inserted into the database table
+                self.todos.append(newlyInsertedItem)
+                
+            } catch {
+                debugPrint(error)
+            }
+        }
     }
     
     func delete(_ todo: TodoItem) {
