05ddf7eb0886ee5a288fe4d9d6d513be00dd2dc2
Irving Meng <imeng@lcs.on.ca>
2024-05-28 09:58:31 -0400

    Specified the sort order so that to-do item positions in the list do not change when to-do items are modified.

--- TodoList/ViewModel/TodoListViewModel.swift
+++ TodoList/ViewModel/TodoListViewModel.swift
@@ -27,6 +27,7 @@ class TodoListViewModel {
             let results: [TodoItem] = try await supabase
                 .from("todos")
                 .select()
+                .order("id" , ascending: true)
                 .execute()
                 .value
             
